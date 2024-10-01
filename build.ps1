d86eccb0ec0ab1b708ff54944acf881ad25f6d9c
deryaza <deryaza@DESKTOP-OBAKE1V>
2024-10-02 01:06:20 +0400

    1

--- build.ps1
+++ build.ps1
@@ -7,10 +7,10 @@ Push-Location src
 Write-Output $PWD
 
 # Build Avalonia LSP
-dotnet build $PWD/AvaloniaLSP/AvaloniaLanguageServer/AvaloniaLanguageServer.csproj /property:GenerateFullPaths=true -c Release --output $PWD/vscode-avalonia/avaloniaServer
+dotnet build $PWD/AvaloniaLSP/AvaloniaLanguageServer/AvaloniaLanguageServer.csproj /property:GenerateFullPaths=true -c Debug --output $PWD/vscode-avalonia/avaloniaServer
 
 # Build  Solution parser
-dotnet build $PWD/SolutionParser/SolutionParser.csproj /property:GenerateFullPaths=true -c Release --output $PWD/vscode-avalonia/solutionParserTool
+dotnet build $PWD/SolutionParser/SolutionParser.csproj /property:GenerateFullPaths=true -c Debug --output $PWD/vscode-avalonia/solutionParserTool
 
 Write-Output 🎉 Great success
 
